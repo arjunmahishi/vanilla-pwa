0932fd84dc820dfad1afaa39151a0d3de6caa1a4
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-12-17 22:10:17 +0530

    Published version 1.0.0

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-const fs = require("fs");
-
-var createStream = fs.createWriteStream("test.txt");
-createStream.end();
+console.log("This is my first node module. Thank you for installing it. \nI will shortly upload this with real code.");
