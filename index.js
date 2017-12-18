ea3d411daa80d17f3feed48b80a6d89431dcf437
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-12-18 16:42:08 +0530

    Made the commands syncronous

--- index.js
+++ index.js
@@ -3,20 +3,25 @@
 const exec = require('child_process').exec;
 const path = require('path');
 
-var run = command => {
-	exec(command, (error, stdout, stderr) => {
+
+if(process.argv.length > 2){
+	console.log("Generating files...")
+
+	exec(`mkdir ${process.argv[2]}`, (error, stdout, stderr) => {
 	    if (error !== null) {
 	        console.log(`exec error: ${error}`);
 	    }else {
-	    	console.log(`${stdout}`);
-		    console.log(`${stderr}`);
+		    exec(`cp -r ${path.resolve(__dirname, 'skeleton/*')} ${process.argv[2]}`, 
+	    	(err, stdout, stderr) =>{
+		    	if(err){
+			        console.log(`exec error: ${error}`);
+		    	}else{
+					console.log(`Your new PWA is hack-ready.\n\nrun`, 
+					`"cd ${process.argv[2]}"\n\nand build something awesome!`);
+		    	}		
+	    	})
 	    }
 	});
-} 
-
-if(process.argv.length > 2){
-	run(`mkdir ${process.argv[2]}`);
-	run(`cp -r ${path.resolve(__dirname, 'skeleton/*')} ${process.argv[2]}`);
 }else{
-
+	console.log('Run "vanilla-pwa <name of the pwa>"')
 }
